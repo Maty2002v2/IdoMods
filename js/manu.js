740a680078ca23c77fbad595653d50bcd6438a77
Maty2002v2 <matikster21@gmail.com>
2025-02-14 13:27:53 +0100

    Basic files and first version menu

---
+++ js/manu.js
@@ -0,0 +1,44 @@
+const toggleMobileMenu = () => {
+    const hamburger = document.querySelector(".hamburger");
+    const navLinks = document.querySelector(".nav-links");
+    const backdrop = document.querySelector(".menu-backdrop");
+    
+    hamburger.addEventListener("click", () => {
+        navLinks.classList.toggle("show");
+        backdrop.classList.toggle("show");
+    });
+
+    // close menu after outside click
+    document.addEventListener("click", (event) => {
+        const isClickInsideMenu = navLinks.contains(event.target) || hamburger.contains(event.target);
+
+        if (!isClickInsideMenu) {
+            navLinks.classList.remove("show");
+            backdrop.classList.remove("show");
+        }
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    const sections = document.querySelectorAll("section");
+    const navLinks = document.querySelectorAll(".nav-links a");
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                navLinks.forEach(link => {
+                    link.classList.remove("active");
+                    if (link.getAttribute("href") === `#${entry.target.id}`) {
+                        link.classList.add("active");
+                    }
+                });
+            }
+        });
+    }, {
+        threshold: 0.7
+    });
+
+    sections.forEach(section => observer.observe(section));
+
+    toggleMobileMenu();
+});
